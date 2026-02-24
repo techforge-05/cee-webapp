57225e3d1a09cd1a17bedf893cf78f91c866a88d
Dixy-05 <ddbarahona89@gmail.com>
2026-02-23 23:44:03 -0600

    update PayPal payment method UI and add Banco Occidente logo

--- app/pages/support/donate.vue
+++ app/pages/support/donate.vue
@@ -149,9 +149,11 @@
           >
             <div class="flex flex-col items-center text-center">
               <div
-                class="w-16 h-16 rounded-full bg-gradient-to-br from-blue-500 to-indigo-500 flex items-center justify-center mb-4"
+                class="w-16 h-16 rounded-full bg-gradient-to-br from-[#003087] to-[#009cde] flex items-center justify-center mb-4"
               >
-                <UIcon name="i-heroicons-currency-dollar" class="w-8 h-8 text-white" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 20 20" class="w-8 h-8">
+                  <path fill="#ffffff" d="M7.914 10.677h1.659c3.604 0 5.649-1.623 6.3-4.96q.032-.164.056-.322c.036-.226.054-.429.062-.624c.006-.134.01-.213.009-.287a2.6 2.6 0 0 0-.216-1.039c-.129-.296-.324-.587-.613-.918C14.318 1.557 12.832 1 11.057 1H5.404a.81.81 0 0 0-.799.683l-1.02 6.571l-1.269 8.185a.486.486 0 0 0 .48.561h2.772l.849-5.043a1.51 1.51 0 0 1 1.497-1.28m9.103-4.587c-.792 3.771-3.357 5.772-7.445 5.772H7.914a.33.33 0 0 0-.328.282L6.481 19h2.907a.71.71 0 0 0 .699-.597l.029-.15l.555-3.514l.036-.194a.71.71 0 0 1 .699-.597h.44c2.85 0 5.081-1.158 5.733-4.506c.268-1.38.132-2.534-.562-3.352"/>
+                </svg>
               </div>
               <h3 class="text-xl font-bold text-gray-900 mb-2">
                 {{ $t('support.donate.payment.methodPaypal.title') }}
